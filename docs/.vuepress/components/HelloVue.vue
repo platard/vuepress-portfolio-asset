4835276b4cacf42600ff7226797ef427e68e27e8
platara <plat0033@algonquinlive.com>
2023-11-12 08:05:04 -0500

    first commit

---
+++ docs/.vuepress/components/HelloVue.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ msg }}</h1>
+    <div class="card">
+        <button type="button" @click="count++">count is {{ count }}</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                count: 2,
+                msg: 'Hello Davo'
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+import { defaultTheme } from 'vuepress'
+
+export default {
+    lang: 'en-US',
+    title: 'VuePress',
+    description: 'Portfolio asset',
+    theme: defaultTheme({
+        // default theme config
+        navbar: [
+          {
+              text: 'Home',
+              link: '/',
+          },
+          {
+            text: 'Guide',
+            link: '/guide'
+          },
+          {
+            text: 'Contact',
+            link: '/contact.md'
+          }
+        ],
+        logo: '/images/iconmonstr-carousel-filled.svg',
+    }),
+}
